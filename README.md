a0ef50a2196b5b978a6895f3c2b9bad6e6bf4f0c
azureru <erwin@lumio.id>
2020-03-01 17:30:43 +0700

    Initial code

---
+++ README.md
@@ -0,0 +1,12 @@
+# Wemos D1 Based Energy Monitor
+
+This is a simple energy monitor using Wemos D1 and cheap non-invasive SCT-013 sensor
+
+## Material
+- Wemos D1
+- USB Cable
+- SCT-013-000
+- 2 10k Ω Resistor
+- 1 22 Ω Resistor
+- 10uF Capacitor
+
---
+++ Wemos-Energymon/src/main.cpp
@@ -0,0 +1,83 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <config.h>
+#include "EmonLib.h"      
+#include <PubSubClient.h>  
+
+EnergyMonitor emon1;        
+unsigned long lastMeasurement = 0;
+unsigned long timeFinishedSetup = 0;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+
+  randomSeed(micros());
+
+  //Initialize emon library
+  emon1.current(A0, 80);  
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.hostname("emon");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // mqtt
+  client.setServer(mqtt_server, 1883);
+}
+
+char msg[50];
+
+void loop() {
+  // mqtt
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  unsigned long currentMillis = millis();  
+  if(currentMillis - lastMeasurement > 1000) {    
+    // emon
+    double Irms = emon1.calcIrms(1480);
+    // the power
+    lastMeasurement = millis();
+    snprintf(msg, 50, "{\"current\": %lf}", Irms);
+    Serial.println(msg);
+    client.publish("emon/current", msg);
+  }
+}
+
